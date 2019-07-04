fe94a029dd5a3c3cefa68b800f1224f351a0bf6f
zwonb <1140114702@qq.com>
2019-07-04 22:02:59 +0800

    11.2.2

---
+++ src/yidont/kotlin/ch11/11.2.2_1.kt
@@ -0,0 +1,45 @@
+package yidont.kotlin.ch11
+
+/**
+ * 说明
+ *
+ * @author zwonb
+ * @date 2019/7/4
+ */
+open class Tag(val name: String) {
+
+    private val children = mutableListOf<Tag>()
+
+    protected fun <T : Tag> doInit(child: T, init: T.() -> Unit) {
+        child.init()
+        children.add(child)
+    }
+
+    override fun toString() =
+            "<$name>${children.joinToString("")}</$name>"
+
+}
+
+class TABLE : Tag("table") {
+    fun tr(init: TR.() -> Unit) = doInit(TR(), init)
+}
+
+class TR : Tag("tr") {
+    fun td(init: TD.() -> Unit) = doInit(TD(), init)
+}
+
+class TD : Tag("td")
+
+fun table(init: TABLE.() -> Unit) = TABLE().apply(init)
+
+fun createTable() = table {
+    tr {
+        td {
+
+        }
+    }
+}
+
+fun main() {
+    println(createTable())
+}
